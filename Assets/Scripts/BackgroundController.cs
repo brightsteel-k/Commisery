d0fc4b19ca9a6bae0f040c5ede04b086e18ecf1f
kate <katezraly@gmail.com>
2023-02-05 00:40:52 -0800

    Added script that generates characters from array of Sprites

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
+
+    GameObject leftPane;
+    GameObject rightPane;
+
+    GameObject firstCharacter;
+
+    GameObject newCharacter;
+    SpriteRenderer cSpriteRenderer;
+
+    public Sprite[] sprites;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        gameObject.transform.position = new Vector3(0, 0, 0);
+
+        leftPane = transform.GetChild(0).gameObject;
+        rightPane = transform.GetChild(1).gameObject;
+        
+        generateCharacter(leftPane, 0f, 0);
         
     }
 
@@ -14,5 +32,36 @@ public class BackgroundController : MonoBehaviour
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.G)) {
+
+            generateCharacter(rightPane, 17.76f, 1);
+
+            translateNewRoom();
+
+        }
+
     }
+
+    void generateCharacter(GameObject appendTo, float xPos, int sIndex) {
+
+        newCharacter = new GameObject();
+
+        newCharacter.name = "Character";
+        newCharacter.transform.localScale = new Vector3(7.4f, 7.4f, 1f);
+        newCharacter.transform.position = new Vector3(xPos, 0f, -0.1f);
+
+        cSpriteRenderer = newCharacter.AddComponent<SpriteRenderer>();
+        cSpriteRenderer.sprite = sprites[sIndex];
+
+        newCharacter.transform.parent = appendTo.transform;
+
+    }
+
+    void translateNewRoom() {
+
+        gameObject.transform.position = new Vector3(0, 0, 0);
+        LeanTween.moveX(gameObject, -17.76f, 4.0f).setEase(LeanTweenType.easeInOutCubic);;
+
+    }
+
 }
