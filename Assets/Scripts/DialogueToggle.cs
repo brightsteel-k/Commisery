0af257b722a9bf923ffaebdc65ee3c88e27930c0
brightsteel-k <kmbrent@email.com>
2023-02-04 23:28:47 -0800

    Fixed dotmanager bugs
    FINALLYY

--- Assets/Scripts/DialogueToggle.cs
+++ Assets/Scripts/DialogueToggle.cs
@@ -13,13 +13,11 @@ public class DialogueToggle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         toggled = false;
 
         dialogue = transform.GetChild(1).gameObject.GetComponent(typeof(TMP_Text)) as TMP_Text;
 
         dialogue.text = "";
-
     }
 
     // Update is called once per frame
